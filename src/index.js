2f532be247b9e40bf2b3f0f9ab335bad671b18f6
summer_tang <cythilya@gmail.com>
2020-08-19 16:31:18 +0800

    change promise lib

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import promise from 'redux-thunk';
+import promise from 'redux-promise';
 import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import { CookiesProvider } from 'react-cookie';
 import './styles/index.scss';
