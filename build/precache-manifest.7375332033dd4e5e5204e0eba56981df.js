e37adae8dae15451ee302ae0d9bfe94c0ebf6a8f
summer_tang <cythilya@gmail.com>
2020-08-19 16:59:17 +0800

    add build files

---
+++ build/precache-manifest.7375332033dd4e5e5204e0eba56981df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb35bdffb64e9bfaab9f8eef11498ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "640624464d39f6cc8c7e",
+    "url": "/static/css/main.4799d698.chunk.css"
+  },
+  {
+    "revision": "496396a9083d0a543841",
+    "url": "/static/js/2.fa26fedf.chunk.js"
+  },
+  {
+    "revision": "52046404584d4e0d436794dfcf23bef2",
+    "url": "/static/js/2.fa26fedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640624464d39f6cc8c7e",
+    "url": "/static/js/main.14f1d32a.chunk.js"
+  },
+  {
+    "revision": "44c9db4eda6b92dfde3a",
+    "url": "/static/js/runtime-main.f84ebeca.js"
+  }
+]);
