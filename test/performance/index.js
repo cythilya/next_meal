7c05303ff6c47b3d9c2ec1fe023c1f152c6b8b63
summer_tang <cythilya@gmail.com>
2020-08-21 18:07:39 +0800

    add fs package for performance report

--- test/performance/index.js
+++ test/performance/index.js
@@ -46,87 +46,88 @@ const TEST_PAGES = [
 ];
 
 (async() => {
-    let browser = null;
-    let page = null;
+  let browser = null;
+  let page = null;
 
-    try {
-        browser = await navigateToPizzaProfile();
-        page = (await browser.pages())[0];
-        const records = [];
+  try {
+    browser = await navigateToIndex();
+    page = (await browser.pages())[0];
+    const records = [];
 
-        console.log('Running Lighthouse...');
+    console.log('Running Lighthouse...');
 
-        for(let i = 0; i < TEST_PAGES.length; i += 1) {
-          const report = await lighthouse(`${ENV}${TEST_PAGES[i].link}`, {
-            port: (new URL(browser.wsEndpoint())).port,
-            output: 'json',
-            logLevel: 'info',
-            disableDeviceEmulation: true,
-            chromeFlags: ['--disable-mobile-emulation']
-          }, config);
-          const json = reportGenerator.generateReport(report.lhr, 'json');
-          const html = reportGenerator.generateReport(report.lhr, 'html');
-          const categories = Object.values(report.lhr.categories);
-          const record = {};
+    for(let i = 0; i < TEST_PAGES.length; i += 1) {
+      const report = await lighthouse(`${ENV}${TEST_PAGES[i].link}`, {
+        port: (new URL(browser.wsEndpoint())).port,
+        output: 'json',
+        logLevel: 'info',
+        disableDeviceEmulation: true,
+        chromeFlags: ['--disable-mobile-emulation']
+    }, config);
+      const json = reportGenerator.generateReport(report.lhr, 'json');
+      const html = reportGenerator.generateReport(report.lhr, 'html');
+      const categories = Object.values(report.lhr.categories);
+      const record = {};
 
-          console.log(`Review results for ${TEST_PAGES[i].id}...`);
-          record['Page'] =`<a href='./lighthouse-results-${TEST_PAGES[i].id}.html' target='blank'>${TEST_PAGES[i].id}</a>`;
+      console.log(`Review results for ${TEST_PAGES[i].id}...`);
+      record['Page'] =`<a href='./lighthouse-results-${TEST_PAGES[i].id}.html' target='blank'>${TEST_PAGES[i].id}</a>`;
 
-          for(let i = 0; i < categories.length; i += 1) {
-            const key = `${categories[i].title}`;
-            record[key] = categories[i].score;
-            console.log(`Lighthouse scores: ${categories[i].title} ${categories[i].score}`);
-          }
+      for(let i = 0; i < categories.length; i += 1) {
+        const key = `${categories[i].title}`;
+        record[key] = categories[i].score;
+        console.log(`Lighthouse scores: ${categories[i].title} ${categories[i].score}`);
+      }
 
-          records.push(record);
+      records.push(record);
 
-          console.log('Writing results...');
-          fs.writeFileSync(`reports/performance/lighthouse-results-${TEST_PAGES[i].id}.json`, json);
-          fs.writeFileSync(`reports/performance/lighthouse-results-${TEST_PAGES[i].id}.html`, html);
-        }
-
-      const headers = {
-        'Page' : 'Page',
-        'Performance': 'Performance',
-        'Accessibility': 'Accessibility',
-        'Best Practices': 'Best Practices',
-        'SEO' : 'SEO',
-        'Progressive Web App': 'Progressive Web App'
-      };
-
-      const table = (new TableBuilder({'class': 'repor-table'}))
-        .setHeaders(headers) // see above json headers section
-        .setData(records) // see above json data section
-        .render()
-
-      fs.writeFileSync(`reports/performance/lighthouse-report-index.html`, table);
-
-      console.log('Done!');
-    } catch (error) {
-      console.error('Error!', error);
-    } finally {
-      await page.close();
-      await browser.close();
+      console.log('Writing results...');
+      fs.writeFileSync(`reports/performance/lighthouse-results-${TEST_PAGES[i].id}.json`, json);
+      fs.writeFileSync(`reports/performance/lighthouse-results-${TEST_PAGES[i].id}.html`, html);
     }
+
+    const headers = {
+      'Page' : 'Page',
+      'Performance': 'Performance',
+      'Accessibility': 'Accessibility',
+      'Best Practices': 'Best Practices',
+      'SEO' : 'SEO',
+      'Progressive Web App': 'Progressive Web App'
+    };
+
+    const table = (new TableBuilder({'class': 'repor-table'}))
+      .setHeaders(headers) // see above json headers section
+      .setData(records) // see above json data section
+      .render()
+
+    fs.writeFileSync(`reports/performance/lighthouse-report-index.html`, table);
+
+    console.log('Done!');
+  } catch (error) {
+    console.error('Error!', error);
+  } finally {
+    await page.close();
+    await browser.close();
+  }
 })();
 
-async function navigateToPizzaProfile() {
+async function navigateToIndex() {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     console.log('Navigating to index...');
-    await page.goto('http://localhost:3000/');
+    await page.goto(ENV);
 
     console.log('Starting login, entering username and password...');
     await page.click('#login');
     await page.type('#account', 'test');
-    await page.type('#password', 'test');page.click('#submit'),
+    await page.type('#password', 'test');
+    await page.click('#submit'),
 
     // console.log('Logging in....');
     // await Promise.all([
     //     page.waitForNavigation({ waitUntil: 'networkidle0' }),
     // ]);
 
-    console.log('Next Meal unlocked!');
+    console.log('Login success!');
     return browser;
 }
